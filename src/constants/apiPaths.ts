9f748acfabd1d52c474520c31edea24b1c0f9c24
Petr Razvaliaev <petr_razvaliaev@epam.com>
2020-11-22 17:32:23 +0400

    Update import service url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   getAllProducts: 'https://43yzo1yuj3.execute-api.eu-west-1.amazonaws.com/develop/products',
   getProductById: 'https://43yzo1yuj3.execute-api.eu-west-1.amazonaws.com/develop/products/{id}',
-  import: 'https://svnq608iz3.execute-api.eu-west-1.amazonaws.com/develop',
+  import: 'https://9nhhji66k6.execute-api.eu-west-1.amazonaws.com/develop',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
