78cf3245c89208bb21af6b3a6c44493aa2265367
Petr Razvaliaev <petr_razvaliaev@epam.com>
2020-12-06 18:07:26 +0400

    task 8. integration with cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   import: 'https://9nhhji66k6.execute-api.eu-west-1.amazonaws.com/develop',
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://petr150894-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
